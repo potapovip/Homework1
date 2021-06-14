699377313dba9f9ecf3634f600684b5bcaae8baf
potapovip <samurai1487@yandex.ru>
2021-06-14 20:52:55 +0500

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/homework1/MainActivity.java
@@ -0,0 +1,55 @@
+package ru.geekbrains.homework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+//    2. Создать ещё один макет (если не получается, то проект) с несколькими EditText,
+//    где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения.
+//3. Добавить в проект элемент CalendarView.
+    private EditText editTextTextPersonName4;
+    private EditText editTextTextPersonName5;
+    private EditText editTextTextEmailAddress;
+    private EditText editTextPhone;
+    private EditText editTextTextPassword;
+    private CalendarView calendarView;
+    private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextTextPersonName4 = findViewById(R.id.editTextTextPersonName4);
+        editTextTextPersonName5 = findViewById(R.id.editTextTextPersonName5);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        calendarView = findViewById(R.id.calendarView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            Person person1 = new Person(editTextTextPersonName4.getText().toString(),editTextTextPersonName5.getText().toString(),editTextTextEmailAddress.getText().toString(), editTextPhone.getText().toString(),editTextTextPassword.getText().toString(),calendarView.getDate());//не понял как правильн получить дату ):
+
+
+
+            Toast toast = Toast.makeText(this, "name: " +person1.name + " " + "lastname: "+ person1.lastName+" ",Toast.LENGTH_LONG);
+            toast.show();
+
+        });
+
+
+
+    }
+
+
+    }
---
+++ app/src/main/java/ru/geekbrains/homework1/Person.java
@@ -0,0 +1,86 @@
+package ru.geekbrains.homework1;
+
+import java.util.Date;
+
+public class Person {
+    String name;
+    String lastName;
+    String email;
+    String phone;
+    String pass;
+    long birtday;
+
+    public  Person(String name,String lastName,String email,String phone,String pass,long birtday){
+         this.name = name;
+         this.lastName = lastName;
+         this.email = email;
+         this.phone=phone;
+         this.pass = pass;
+         this.birtday = birtday;
+    }
+    public Person(String name){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public long getBirtday() {
+        return birtday;
+    }
+
+    public void setBirtday(long birtday) {
+        this.birtday = birtday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", pass='" + pass + '\'' +
+                ", birtday=" + birtday +
+                '}';
+    }
+
+}
